705211e0541986b3eab17d60ec2e44800868b10b
Navid_16 <77239915+Navid-16@users.noreply.github.com>
2021-07-04 11:49:56 +0300

    Add files via upload

---
+++ Discord bot setup/commands/slap.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: 'slap',
+    description: 'A command to slap someone',
+    execute(message, Discord) {
+
+        const random = [
+            '( ;´0｀)ﾂ))□｀)',
+            '(* ＇Д＇)ﾉｼ)ﾟﾛﾟ)',
+            '(￣ε(#￣)☆╰o(￣▽￣///)'
+        ];
+
+        let member = message.mentions.members.first();
+        var randomtext = Math.floor(Math.random() * random.length);
+
+        if (!member) {
+            message.channel.send(`${message.author}, no user found...`)
+        } else {
+            try {
+                const embed = new Discord.MessageEmbed()
+
+                    .setColor('RANDOM')
+                    .setFooter('Made by Navid_16#6966 with ❤️')
+                    .setDescription(`${message.author} ${random[randomtext]} ${member}`)
+
+                message.channel.send(embed);
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
+}
---
+++ Discord bot setup/events/leave.js
@@ -0,0 +1,30 @@
+module.exports = client => {
+
+    const Discord = require('discord.js');
+
+    
+
+    client.on('guildMemberRemove', async member => {
+
+		const guild = client.guilds.cache.get("")/** <-- Add your serverID */
+		let channelID = '';/** <-- Add the channelID*/
+		let server = member.guild.name;
+
+
+		try{
+    
+			 const embed = new Discord.MessageEmbed()
+
+			 .setDescription(`${member} HAS LEFT ${server}...`)
+			 .setColor('RANDOM')
+			 .setFooter('Made by Navid_16#6966 with ❤️')
+			 .setTimestamp(Date())
+			 .setAuthor(member.user.username, member.user.displayAvatarURL())
+			 .setThumbnail(member.user.displayAvatarURL())
+
+			member.guild.channels.cache.find(t => t.id === channelID).send(embed);
+		}catch(err){
+			console.log(err)
+		}
+       
+})}
