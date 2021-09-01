bc30bb485e4e814c74d8367d021e07eb9cbc8791
Navid_16 <77239915+Navid-16@users.noreply.github.com>
2021-09-01 17:36:37 +0300

    Delete Discord bot setup directory

--- Discord bot setup/events/addroles.js
+++
@@ -1,9 +0,0 @@
-module.exports  = client => {
-
-  const setup = require('../setup');
-
-    client.on('guildMemberAdd', member =>{
-        var role = member.guild.roles.cache.find(r => r.name === setup.AddRoles());
-        member.roles.add(role)
-    });
-}
